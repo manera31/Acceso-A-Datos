0e54c7e9bd8c9b147fa583ef4a0842a31d29b66d
Joan Manera <joanet31@hotmail.com>
2019-09-19 13:10:09 +0200

    Escribir File

--- src/main/java/com/joanmanera/EscribirFile.java
+++ src/main/java/com/joanmanera/EscribirFile.java
@@ -5,25 +5,26 @@ import java.util.Scanner;
 
 public class EscribirFile {
     public static void main(String[] args) throws IOException {
-        File f = new File("/home/joan/IdeaProjects/Acceso a datos/hola.txt");
+        File f = new File("C:\\Users\\joane\\Desktop\\Joan\\2 DAM\\Acceso a datos\\AccesoADatos\\hola.txt");
         FileWriter fw = new FileWriter(f, true);
         Scanner lector = new Scanner(System.in);
         char c;
         boolean b = true;
         do {
-            System.out.println("Escribe letra: ");
+            System.out.print("Escribe letra: ");
             c = lector.nextLine().charAt(0);
             if(c == '\\'){
                 b=false;
             } else {
-                fw.write(c);
+                fw.write((char)c);
             }
         } while (b);
-
+        fw.close();
         FileReader fr = new FileReader(f);
         int i;
         while ((i = fr.read()) != -1){
             System.out.print((char)i);
         }
+
     }
 }
--- src/main/java/com/joanmanera/LeerFile.java
+++ src/main/java/com/joanmanera/LeerFile.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class LeerFile {
     public static void main(String[] args) throws IOException {
-        File f = new File("/home/joan/IdeaProjects/Acceso a datos/hola.txt");
+        File f = new File("C:\\Users\\joane\\Desktop\\Joan\\2 DAM\\Acceso a datos\\AccesoADatos\\hola.txt");
         FileReader fr = new FileReader(f);
         int i;
         while ((i = fr.read()) != -1){
